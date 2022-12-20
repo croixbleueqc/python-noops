04db6d0c678abe8da853e54b5172218a9a529bdd
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-20 19:58:40 +0000

    Adding tarfile member sanitization to extractall()

--- noops/package/install.py
+++ noops/package/install.py
@@ -156,7 +156,26 @@ class HelmInstall():
         logging.debug("untar local package %s", os.fspath(pkg))
 
         with tarfile.open(pkg, "r:gz") as tar:
-            tar.extractall(dst)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, dst)
 
         return list(dst.glob("*"))[0]
 
